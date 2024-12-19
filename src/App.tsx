5cf4990ebb5425492b58e154d485a4a7b1011b02
Praksix <nathan.vianey@gmail.com>
2024-12-19 11:59:40 +0100

    premiere version du front

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <section className="hero is-fullheight is-default is-bold">
+        <div className="hero-head">
+            <nav className="navbar">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="../">
+                            <img src="./src/assets/img/lgo.svg" alt="Logo"/>
+                        </a>
+                        <span className="navbar-burger burger" data-target="navbarMenu">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+                    <div id="navbarMenu" className="navbar-menu">
+                        <div className="navbar-end">
+                            <div className="tabs is-right">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div className="hero-body">
+            <div className="container has-text-centered">
+                <div className="columns is-vcentered">
+                    <div className="column is-5">
+                        <figure className="image is-4by3">
+                            <img src="./src/assets/img/FamilySnap.svg" alt="Description"/>
+                        </figure>
+                    </div>
+                    <div className="column is-6 is-offset-1" id="glasslp">
+                        
+                        <p className="white">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec  mauris turpis. Praesent id sapien semper, posuere eros in, faucibus  urna. Vestibulum condimentum sapien varius, malesuada metus in, egestas  nibh.
+                            <br/>
+                            <a href="create.html"><button className="button" id="create">Create an Event</button></a>
+                            <p className="has-text-centered or">or</p>
+                            <input className="input" type="text" placeholder="Paste Your Code" />
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     </>
   )
 }
