90ed90208ffc349c22064124c705b2625ddd9350
j-mmyni <j.ni@betclicgroup.com>
2024-12-19 17:53:23 +0100

    feat: adding "add to home screen" pwa functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import  CreateEvent  from './pages/CreateEvent';
 import HomePage from "./pages/HomePage.tsx";
 import Feed from "./pages/Feed.tsx";
 import Share from "./pages/Share.tsx";
+import AddToHomeScreen from "./components/BeforeInstallPrompt.tsx";
 
 
 
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <>
+        <AddToHomeScreen />
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/create" element={<CreateEvent />} />
