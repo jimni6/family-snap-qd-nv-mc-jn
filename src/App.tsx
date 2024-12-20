720d653bfc8d9d699e0146eb935e271eb0ad03a6
j-mmyni <j.ni@betclicgroup.com>
2024-12-20 11:32:23 +0100

    fix: try to fix refresh app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import  CreateEvent  from './pages/CreateEvent';
 import HomePage from "./pages/HomePage.tsx";
 import Feed from "./pages/Feed.tsx";
 import Share from "./pages/Share.tsx";
-import AddToHomeScreen from "./components/BeforeInstallPrompt.tsx";
 import Add from "./pages/Add.tsx";
 
 
@@ -13,7 +12,7 @@ function App() {
 
   return (
     <>
-        <AddToHomeScreen />
+        {/*<AddToHomeScreen />*/}
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/create" element={<CreateEvent />} />
